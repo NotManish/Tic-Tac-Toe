5a4ee836658b017ac85f8e9222c6636e112284be
NotManish <manishshrestha051@gmail.com>
2025-01-07 15:32:39 +0545

    modified vite config file to run the application on mobile devices in the network

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',  // Makes the server accessible to other devices on the network
+    port: 3000
+  },
   plugins: [react()],
-})
+});
